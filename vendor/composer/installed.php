0480d1c4a1a94ee459336f6c3d8d0f4ed25ef6d5
belpaco <frp3826@students.uc-bcf.edu.ph>
2025-04-10 16:48:46 +0800

    new mailer

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '32194a75ef2eacf639d70aba9f17626ffa5abced',
+        'reference' => '5f9797784948de66a3027afdd45e1d3dbfe7852b',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '32194a75ef2eacf639d70aba9f17626ffa5abced',
+            'reference' => '5f9797784948de66a3027afdd45e1d3dbfe7852b',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
